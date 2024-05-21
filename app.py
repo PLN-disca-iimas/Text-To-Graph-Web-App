2557573a4c66ab73c757d9dee9246389ed7ccc62
Diego AB <diegoavendano1998@gmail.com>
2024-05-21 10:26:50 -0600

    Update app port

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from src import app
 from src.routes.graph_text_routes import *
+from src.routes.assets_routes import *
   
   
 if __name__ == "__main__":
-    app.run(host ='0.0.0.0', port = 6000, debug = True)
+    app.run(host ='0.0.0.0', port = 5000, debug = True)
