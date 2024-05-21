98f5ba6659dcb68500d4811210c4e5293af9492a
Diego AB <diegoavendano1998@gmail.com>
2024-05-21 10:27:25 -0600

    Add constants file

---
+++ src/constants.py
@@ -0,0 +1,25 @@
+ROOT_ABSOLUTE_PATH = "/home/diego/proyects/Text-To-Graph-Web-App"
+GRAPH_OUTPUT_DIRECTORY = "src/static/outputs/graphs/"
+
+ARROW_STYLES = [
+    ("-", "Curve"),
+    ("->", "CurveB"),
+    ("-[", "BracketB"),
+    ("-|>", "CurveFilledB"),
+    ("<-", "CurveA"),
+    ("<->", "CurveAB"),
+    ("<|-", "CurveFilledA"),
+    ("<|-|>", "CurveFilledAB"),
+    ("]-", "BracketA"),
+    ("]-[", "BracketAB"),
+    ("fancy", "Fancy"),
+    ("simple", "Simple"),
+    ("wedge", "Wedge"),
+    ("|-|", "BarAB"),
+]
+COOCURRENCE = "Coocurrence"
+HETEROGENEUS = "Heterogeneus"
+GRAPH_TYPES = [
+    (COOCURRENCE, "Coocurrencia"),
+    (HETEROGENEUS, "Heterogenea")
+]
