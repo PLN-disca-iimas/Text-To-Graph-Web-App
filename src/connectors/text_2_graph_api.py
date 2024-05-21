7708a05d15e2d400761ee5c9e176e35b8f7db926
Diego AB <diegoavendano1998@gmail.com>
2024-05-21 10:27:41 -0600

    Get Heterofgeneus graph

--- src/connectors/text_2_graph_api.py
+++ src/connectors/text_2_graph_api.py
@@ -1,4 +1,5 @@
 from text2graphapi.text2graphapi.src.Cooccurrence import Cooccurrence
+from text2graphapi.text2graphapi.src.Heterogeneous import Heterogeneous
 import nltk
 nltk.download('omw-1.4')
 
@@ -16,3 +17,29 @@ class Text2GraphApiconnector:
         to_word_coocc_graph_transformed = to_word_coocc_graph.transform(corpus_docs)
 
         return to_word_coocc_graph_transformed
+
+    @staticmethod
+    def get_coocurrence_graph() -> Cooccurrence:
+        coocurrence_graph = Cooccurrence(
+            graph_type='DiGraph', 
+            language='en', 
+            window_size=3, 
+            output_format='adj_matrix'
+        )
+        return coocurrence_graph
+
+    @staticmethod
+    def get_heterogeneus_graph() -> Heterogeneous:
+        to_hetero_graph = Heterogeneous(
+            graph_type = 'Graph', 
+            window_size = 20,
+            language = 'en', 
+            output_format = 'networkx'
+        )
+        return to_hetero_graph
+
+    @staticmethod
+    def get_transformed_text(coocurrence_graph:Cooccurrence, corpus_docs: list) -> list:
+        coocurrence_graph_transformed = coocurrence_graph.transform(corpus_docs)
+
+        return coocurrence_graph_transformed
