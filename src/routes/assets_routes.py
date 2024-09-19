cb61fa1f05855df588193f5bb153855d3e33b514
diegoavendanozeb <diego.avendano@zeb.mx>
2024-09-19 12:59:40 -0600

    Feat: Document API routes

--- src/routes/assets_routes.py
+++ src/routes/assets_routes.py
@@ -10,10 +10,30 @@ load_dotenv()
 
 @app.route('/assets/<filename>/', methods=['GET'])
 def assets_manager(filename):
+    """
+    Route handler for serving static assets.
 
+    Args:
+        filename (str): The name of the file to be served.
+
+    Returns:
+        Response: The file specified by the filename parameter.
+
+    Raises:
+        NotFound: If the file specified by the filename parameter does not exist.
+
+    """
     return send_from_directory(os.path.join(os.getenv("ROOT_ABSOLUTE_PATH", "/"), 'src', 'static'), filename.replace("&","/"))
 
 @app.route('/download-file/<filename>/', methods=['GET'])
 def download_asset(filename):
+    """
+    Download the specified asset file.
 
+    Args:
+        filename (str): The name of the file to be downloaded.
+
+    Returns:
+        Response: The file to be downloaded as an attachment.
+    """
     return send_file(os.path.join(os.getenv("ROOT_ABSOLUTE_PATH", "/"), 'src', 'static', filename.replace("&","/")), as_attachment=True)
--- src/routes/graph_text_routes.py
+++ src/routes/graph_text_routes.py
@@ -3,22 +3,47 @@ from flask import render_template, request, url_for, redirect, flash
 from src import app
 from src.services.graph_text_service import GraphTextService
 from src.models.base_graph import BaseGraphForm
-  
+
 @app.route('/test/', methods=["GET"])
 def test():
+    """
+    Route that returns the result of the 'get_test' method from the GraphTextService.
+
+    Returns:
+        str: The result of the 'get_test' method.
+    """
     return str(GraphTextService.get_test())
-  
+
 @app.route('/', methods=["GET"])
 def index():
+    """
+    Route that redirects to the 'graph_form' route.
+
+    Returns:
+        redirect: Redirects to the 'graph_form' route.
+    """
     return redirect(url_for("graph_form"))
-  
+
 @app.route('/graph-form/', methods=["GET"])
 def graph_form():
+    """
+    Route that renders the graph form template.
+
+    Returns:
+        render_template: The rendered template with the necessary variables.
+    """
     form = BaseGraphForm(meta={"csrf": False})
     return render_template("graph_form.html", **locals())
 
 @app.route('/generate-graph/', methods=["POST"])
 def generate_graph():
+    """
+    Route that generates a graph based on the form data and file uploaded.
+
+    Returns:
+        redirect: Redirects to the 'graph_form' route if the graph generation fails, 
+                  otherwise redirects to the 'graph_plot' route with the generated graph's document name.
+    """
     form = BaseGraphForm(meta={"csrf": False})
 
     if len(request.form.get("text", "")) or request.files.getlist(form.file.name)[0]:
@@ -39,6 +64,15 @@ def generate_graph():
 
 @app.route('/graph-plot/<docname>', methods=["GET"])
 def graph_plot(docname):
+    """
+    Route that renders the graph plot template and passes the necessary variables for displaying the graph.
+
+    Args:
+        docname (str): The name of the document.
+
+    Returns:
+        render_template: The rendered template with the necessary variables.
+    """
     graph_code = GraphTextService.get_graph_code(docname)
     text_graph = GraphTextService.get_text_graph(docname)
     original_text = GraphTextService.get_original_text(docname)
